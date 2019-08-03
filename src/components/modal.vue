f0b451ee3c741d816ba7169e74ec2961f8f43909
canhchimlac <canhchimlac.94@gmail.com>
2019-08-03 19:39:51 +0700

    add new file

---
+++ src/components/modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal-waper">
+    <div class="mdl-container">
+      <div class="mdl-header">
+        <div class="mdl-title" >
+            <span>{{ title }}</span>
+        </div>
+        <div class="btl-close">
+            <span>Close</span>
+        </div>
+      </div>
+      <div class="mdl-body">
+        <slot name="conten"></slot>
+      </div>
+      <div class="mdl-footer">
+          <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+    props: {
+        title: {
+          type: String,
+          default: "Modal"
+        }
+
+    },
+    data: {
+
+    }
+}
+</script>
+
